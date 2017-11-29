5468d18538ffe043077a0f33be2911d02abcf811
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-29 15:29:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,19 @@
+# # code your #position_taken? method here!
+# def position_taken?(board, index)
+#   if board[index] == "  " || board[index] == " " || board[index] == nil
+#     return false
+#   elsif board[index] == "X" || board[index] == "O"
+#     return true
+#   end
+# end
+
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == "  " || board[index] == " " || board[index] == nil
+  if board[index] == "  "
+    return false
+  elsif board[index] == " "
+    return false
+  elsif board[index] == nil
     return false
   elsif board[index] == "X" || board[index] == "O"
     return true
