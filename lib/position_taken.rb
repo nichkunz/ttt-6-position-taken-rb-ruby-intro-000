a0bb3225e13b0c0f8facdf6cbc0a33b82ff335a9
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-29 15:30:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -13,6 +13,8 @@ def position_taken?(board, index)
     return false
   elsif board[index] == " "
     return false
+  elsif board[index] == ""
+    return false
   elsif board[index] == nil
     return false
   elsif board[index] == "X" || board[index] == "O"
